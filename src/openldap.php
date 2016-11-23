ccecf040717e6e816eb270c7c62c1ebd4876f485
Toriq Bagus <toriqbagus@gmail.com>
2016-11-23 09:02:58 +0700

    update ldap auth

--- src/openldap.php
+++ src/openldap.php
@@ -6,38 +6,72 @@
  * @license  http://opensource.org/licenses/MIT
  * @package  PHP openLDAP
  */
+
+use Log;
  
 class OpenLDAP 
 {
+    private $connection
+
+    public function __construct()
+    {
+        $host = config('ldap.host');
+        $port = config('ldap.port');
+
+        $this->connection = $this->connect($host, $port);
+    }
+
+    public function __destruct()
+    {
+        if (! is_null($this->connection))
+            ldap_unbind($this->connection);
+    }
+
     /**
      * Set the connection to LDAP server.
      *
-     * @param string $server
+     * @param string $host
      * @param string $port
      */
-    function connect($server, $port) 
+    function connect($host, $port) 
     {
         $connection = ldap_connect($server, $port);  // must be a valid LDAP server! 
         ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
  
         // PHP Reference says there is no control of connection status in OpenLDAP 2.x.x
         // So we'll use binding function to check connection status.
- 
         return $connection;
  
+    }
+
+    /**
+     * Authenticate to LDAP server.
+     *
+     * @param string $username
+     * @param string $password
+     */
+    public function authenticate($username, $password)
+    {
+        if (empty($username) or empty($password)) {
+            Log::error('Error binding to LDAP: username or password empty');
+            return false;
+        }
+
+        $ldapRdn = config('ldap.login_attribute') . "=" . $username . "," . config('ldap.basedn');
+        $isConnected = $this->bind($this->connection, $ldapRdn, $password);
+
+        return $isConnected;
     }
     
     /**
      * Set the connection to LDAP server.
      *
      * @param string $connection
-     * @param string $basedn
-     * @param string $basepass
+     * @param string $ldaprdn
+     * @param string $password
      */
-    function bind($connection, $basedn, $basepass)
+    function bind($connection, $ldaprdn, $password)
     {
-        $ldaprdn  = $basedn;    // ldap rdn or dn 
-        $ldappass = $basepass;  // associated password
         $bind = ldap_bind($connection, $ldaprdn, $ldappass);
  
         if ($bind) { 
@@ -55,12 +89,12 @@ class OpenLDAP
      * @param string $filter
      * @param array $attributes
      */
-    function search($connection, $searchdn, $filter, $attributes = array())
+    public function search($searchdn, $filter, $attributes = array())
     {
-        $search = ldap_search($connection, $searchdn, $filter, $attributes);
+        $search = ldap_search($this->connection, $searchdn, $filter, $attributes);
  
         if ($search) {
-            $info = ldap_get_entries($connection, $search);
+            $info = ldap_get_entries($this->connection, $search);
  
             return $info;
         } 
@@ -72,12 +106,11 @@ class OpenLDAP
     /**
      * Get count data.
      *
-     * @param string $connection
      * @param string $searchdn
      */    
-    function countData($connection, $search)
+    public function countData($search)
     {
-        $count = ldap_count_entries($connection, $search);
+        $count = ldap_count_entries($this->connection, $search);
 
         return $count;
 
@@ -90,7 +123,7 @@ class OpenLDAP
      * @param string $adddn
      * @param array $record
      */
-    function addRecord($connection, $adddn, $record)
+    public function addRecord($connection, $adddn, $record)
     {
         $addProcess = ldap_add($connection, $adddn, $record);
 
@@ -109,7 +142,7 @@ class OpenLDAP
      * @param string $modifydn
      * @param array $record
      */
-    function modifyRecord($connection, $modifydn, $record)
+    public function modifyRecord($connection, $modifydn, $record)
     {
         $modifyProcess = ldap_modify($connection, $modifydn, $record);
         
@@ -128,7 +161,7 @@ class OpenLDAP
      * @param string $dn
      * @param boolean $recursive
      */
-    function deleteRecord($connection, $dn, $recursive = false)
+    public function deleteRecord($connection, $dn, $recursive = false)
     {
         if ($recursive == false) { 
             return (ldap_delete($connection, $dn));
@@ -156,12 +189,67 @@ class OpenLDAP
      *
      * @param string $connection
      */
-    function close($connection) 
+    public function close($connection) 
     {
         ldap_close($connection);
 
         return true;
+    }
 
+    /**
+     * Get user data from LDAP server.
+     *
+     * @param string $identifier
+     * @param string $attr
+     */
+    public function getUserData($identifier, $attr='')
+    {
+        $ldapFilter = "(&(" . config('ldap.login_attribute') . "=". $identifier . "))";
+        if (!is_array($attr)) {
+            $attr = array();
+        }
+
+        $userInfo = $this->search($this->connection, config('ldap.basedn'), $ldapFilter, $attr);
+    
+        if ($userInfo) {
+            return $userInfo[0];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get group list from LDAP server.
+     */
+    public function getGroupList()
+    {
+        $ldapFilter = "(cn=*)";
+        $attr = array("cn", "gidNumber");
+        $info = $this->search($this->connection, config('ldap.groupdn'), $ldapFilter, $attr);
+
+        $groupList = array();
+        foreach ($info as $each)
+        {
+            if (!empty($each["cn"][0]))
+                $groupList[] = $each["cn"][0];
+        }
+
+        return $groupList;
+    }
+
+    /**
+     * Get which group from user.
+     */
+    public function whichGroup($identifier)
+    {
+        $gidnumber = strval($this->getUserData($identifier)['gidnumber'][0]);
+
+        return $this->groupList[$gidnumber];
+    }
+
+    public function groupIsOK()
+    {
+        return false;
     }
  
 }
